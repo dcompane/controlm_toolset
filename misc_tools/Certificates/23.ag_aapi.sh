65c75f97d899000952b941a24b3d9d552d6265ad
dcompane <dcompane@gmail.com>
2025-11-25 16:01:30 -0800

    Zone 3 AAPI

---
+++ misc_tools/Certificates/23.ag_aapi.sh
@@ -0,0 +1,127 @@
+#! /bin/bash
+
+# BSD 3-Clause License
+
+# Copyright (c) 2021, 2025, BMC Software, Inc.; Daniel Companeetz
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# SPDX-License-Identifier: BSD-3-Clause
+# For information on SDPX, https://spdx.org/licenses/BSD-3-Clause.html
+
+# Change Log
+# Date (YMD)    Name                  What
+# --------      ------------------    ------------------------
+# 20251125      Daniel Companeetz     Initial release
+
+set -x
+
+agent_logical="dc01-docker-b72a5"
+agent_ctms="dc01"
+
+# This script is for the Control-M Agent Zone 3
+# Can be used to deploy a new certificate to an agent using AAPI.
+
+# Needs access to the intCA if will sign. Otherwise, it will be two scripts
+base_dir=`pwd`
+base_dir="${base_dir}/.."
+
+IntCA_dir=${base_dir}/intCA
+
+
+file_name=$agent_logical-$agent_ctms
+csr_file=$IntCA_dir/csr/$file_name.csr
+crt_file=$IntCA_dir/certs/$file_name.crt
+
+# This will use the AAPI method.
+
+cd $base_dir
+#reuse the file name. Change the name to something not unique if you will multi-thread
+cat <<EOF > csr_params.json
+
+{
+   "organization": "DCO",
+   "organizationUnit": "DCO CTM",
+   "cityLocality": "Oakland",
+   "stateProvince": "CA",
+   "country": "US",
+   "emailAddress": "$agent_logical@$agent_ctms"
+}
+
+EOF
+
+# Create the CSR
+ctm config server:agent:csr::create $agent_ctms $agent_logical -f csr_params.json | tee $csr_file
+
+# Verify the CSR
+/bin/openssl req -verify -in $csr_file -text
+
+
+
+#Sign the CSR with the intermediate certificate
+#   This is normally done by the Security team or online.
+
+
+#Revoke existing certificate
+if [ -e "$crt_file" ];then
+    openssl ca -revoke $crt_file \
+        -config ${IntCA_dir}/opensslintCA.cnf \
+        -passin file:${IntCA_dir}/private/.ctmintCA.key.passwd
+fi
+
+#Sign it with the Intermediate CA
+openssl ca -config ${IntCA_dir}/opensslintCA.cnf -extensions server_cert \
+    -days 375 -notext -md sha256 \
+    -in $csr_file \
+    -out $crt_file -batch \
+    -passin file:${IntCA_dir}/private/.ctmintCA.key.passwd
+
+#5 Verify the certificate
+openssl x509  -in $crt_file -text
+openssl verify -untrusted ${IntCA_dir}/certs/ctmintCA.crt \
+        -CAfile ${IntCA_dir}/../rootCA/certs/ctmrootCA.crt \
+        $crt_file
+
+# see https://documents.bmc.com/supportu/API/Monthly/en-US/Documentation/API_Services_ConfigService_Agent.htm#config106
+
+# Show some commands for changing parameters. Useful to know.
+#      Do not set unless needed as it may impact server performance if set for all agents.
+ctm config server:agent::update $agent_ctms $agent_logical persistentConnection Y
+ctm config server:agent::update $agent_ctms $agent_logical allowAgentDisconnection N
+
+# Deploy the certificate
+ctm config server:agent:crt::deploy $agent_ctms $agent_logical $crt_file ${IntCA_dir}/certs/ctmchainCA.pem
+
+# Enable SSL 
+ctm config server:agent::update $agent_ctms $agent_logical sslState Enabled
+
+#verify the expiration date of the certificate deployed
+ctm config server:agent:crt:expiration::get $agent_ctms $agent_logical
+
+# Recycle the agent to pick up the new certificate
+# https://documents.bmc.com/supportu/API/Monthly/en-US/Documentation/API_Services_ConfigService_Agent.htm#config79
+ctm config item::recycle "CTMS:Agent:$agent_ctms:$agent_logical"
